74c00cbf6308913412a4512b43e595b60515c9cf
Payson Holmes <paysonholmes@yahoo.com>
2022-05-31 12:00:38 -0600

    v1.0.1 STABLE

---
+++ README.md
@@ -0,0 +1,5 @@
+# Purple Haze - Phantom Forces
+open source script for phantom forces
+
+![img1](https://i.imgur.com/k9jel15.png)
+![img2](https://i.imgur.com/3OKP7nN.png)
---
+++ loader.lua
@@ -0,0 +1,27 @@
+local script_details = {
+    debug = false,
+    version = "1.0.0",
+}
+
+local url = script_details.debug and "http://localhost:6845" or "https://raw.githubusercontent.com/P-DennyGamingYT/ALCATRAZ-PF/main"
+
+local out = script_details.debug and function(T, ...)
+    return warn("[ALCATRAZ - DEBUG]: "..T:format(...))
+end or function() end
+
+local function import(file)
+    out("Importing File \"%s\"", file)
+    -- return task.spawn(function()
+    local x, a = pcall(function()
+        return loadstring(game:HttpGet(url .. file))()
+    end)
+    if not x then
+        return warn('failed to import', file)
+    end
+    -- end)
+end
+
+getgenv().import = import
+getgenv().details = scriptdetails
+
+import('/main.lua')
