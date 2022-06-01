9a220384b4c9a73a44f52f2616d752f8bfa2bfff
Payson Holmes <paysonholmes@yahoo.com>
2022-05-31 22:19:16 -0600

    v1.0.1 STABLE

--- ui/load_window.lua
+++ ui/load_window.lua
@@ -9,7 +9,6 @@ local window = library:CreateWindow(
 
 local aimbot_tab = window:CreateTab("MAIN")
 local character_tab = window:CreateTab("PLAYER MODS")
-local credits_tab = window:CreateTab("CREDITS")
 do
     local fov_circle = Drawing.new("Circle")
     fov_circle.Thickness = 1
@@ -28,80 +27,80 @@ do
         end
     end)
     
-    local silentaim_sector = aimbot_tab:CreateSection("silent aim")
-    silentaim_sector:CreateToggle("enabled", false, function(state)
+    local silentaim_sector = aimbot_tab:CreateSection("SILENT AIM")
+    silentaim_sector:CreateToggle("TRUE", false, function(state)
         config.aimbot.silent_aim = state
     end)
-    silentaim_sector:CreateDropdown("hit part", {
-        "head",
-        "torso"
+    silentaim_sector:CreateDropdown("HIT PART", {
+        "HEAD",
+        "TORSO"
     }, function(state)
         config.aimbot.target_part = state
     end)
-    silentaim_sector:CreateSlider("hit chance %", 0, 100, 100, true, function(state)
+    silentaim_sector:CreateSlider("HIT CHANCE", 0, 100, 100, true, function(state)
         config.aimbot.hit_chance = state
     end)
-    local fieldofview_sector = aimbot_tab:CreateSection("field of view")
-    fieldofview_sector:CreateToggle("enabled", false, function(state)
+    local fieldofview_sector = aimbot_tab:CreateSection("FOV")
+    fieldofview_sector:CreateToggle("TRUE", false, function(state)
         config.aimbot.field_of_view = state
     end)
-    fieldofview_sector:CreateSlider("range", 0, 360, 180, true, function(state)
+    fieldofview_sector:CreateSlider("RANGE", 0, 360, 180, true, function(state)
         config.aimbot.field_of_view_range = state
         fov_circle.Radius = state
     end)
-    fieldofview_sector:CreateColorpicker("color", function(state)
+    fieldofview_sector:CreateColorpicker("COLOR", function(state)
         fov_circle.Color = state
     end)
-    fieldofview_sector:CreateToggle("visible", false, function(state)
+    fieldofview_sector:CreateToggle("VISIBLE", false, function(state)
         fov_circle.Visible = state
     end)
-    fieldofview_sector:CreateToggle("filled", false, function(state)
+    fieldofview_sector:CreateToggle("SOLID", false, function(state)
         fov_circle.Filled = state
     end)
-    fieldofview_sector:CreateSlider("transparency", 0, 1, 1, false, function(state)
+    fieldofview_sector:CreateSlider("TRANSPARENCY", 0, 1, 1, false, function(state)
         fov_circle.Transparency = state
     end)
-    local gunmod_sector = aimbot_tab:CreateSection("gun mod")
-    gunmod_sector:CreateToggle("fast reload", false, function(state)
+    local gunmod_sector = aimbot_tab:CreateSection("GUN MODS")
+    gunmod_sector:CreateToggle("INSTANT RELOAD", false, function(state)
         config.gunmod.fast_reload = state
     end)
-    gunmod_sector:CreateToggle("fast equip", false, function(state)
+    gunmod_sector:CreateToggle("INSTANT EQUIP", false, function(state)
         config.gunmod.fast_equip = state
     end)
 end
 do
-    local movement_sector = character_tab:CreateSection("movement")
-    movement_sector:CreateToggle("walkspeed", false, function(state)
+    local movement_sector = character_tab:CreateSection("MOVEMENT")
+    movement_sector:CreateToggle("WALKSPEED", false, function(state)
         config.character.walkspeed = state
     end)
-    movement_sector:CreateToggle("jumppower", false, function(state)
+    movement_sector:CreateToggle("JUMPOWER", false, function(state)
         config.character.jumppower = state
     end)
-    movement_sector:CreateToggle("auto deploy", false, function(state)
+    movement_sector:CreateToggle("AUTO DEPLOY", false, function(state)
         config.character.auto_deploy = state
     end)
-    movement_sector:CreateToggle("fake lag ( might be buggy )", false, function(state)
+    movement_sector:CreateToggle("LAG SERVER", false, function(state)
         config.character.fake_lag = state
     end)
     local settings_sector = character_tab:CreateSection("settings")
-    settings_sector:CreateSlider("walkspeed amount", 0, 100, 35, true, function(state)
+    settings_sector:CreateSlider("WALKSPEED", 0, 100, 35, true, function(state)
         set_speed(state)
     end)
-    settings_sector:CreateSlider("jumppower amount", 0, 100, 35, true, function(state)
+    settings_sector:CreateSlider("JUMPOWER", 0, 100, 35, true, function(state)
         set_jump_power(state)
     end)
-    settings_sector:CreateSlider("fakelag amount", 0, 20, 15, true, function(state)
+    settings_sector:CreateSlider("LAG", 0, 20, 15, true, function(state)
         config.character.fake_lag_limit = state
     end)
     
-    local antiaim_sector = character_tab:CreateSection("anti aim")
-    antiaim_sector:CreateToggle("enabled", false, function(state)
+    local antiaim_sector = character_tab:CreateSection("ANTI AIM")
+    antiaim_sector:CreateToggle("TRUE", false, function(state)
         config.character.antiaim = state
     end)
-    antiaim_sector:CreateDropdown("stance type", {
-        "prone",
-        "crouch",
-        "stand"
+    antiaim_sector:CreateDropdown("ANTI AIM METHOD", {
+        "PRONE",
+        "CROUCH",
+        "STAND"
     }, function(state)
         config.character.antiaim_stance = state
     end)
